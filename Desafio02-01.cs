c218981434eee86dc5c0c6a9316f51d8e312b3f4
Carlos-250901 <51840942+Carlos-250901@users.noreply.github.com>
2020-01-22 19:12:59 -0500

    Add files via upload

---
+++ Desafio02-01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ingreso de datos
+        Console.WriteLine("Ingrese variables y y z");
+        double y = double.Parse(Console.ReadLine());
+
+        double z = double.Parse(Console.ReadLine());
+
+        //formulas de t,a,c
+        double t = Math.Sqrt(y * y + z * z);
+        Console.WriteLine("t es");
+        Console.WriteLine(t);
+        
+
+        double c = Math.Acos(y / t)*(180.0 / Math.PI);
+       
+        Console.WriteLine("c es");
+        
+        Console.WriteLine(c);
+        double a = Math.Acos( z / t)*(180.0 / Math.PI);
+       
+        Console.WriteLine("a es");
+        Console.WriteLine(a);
+    }
+
+}
