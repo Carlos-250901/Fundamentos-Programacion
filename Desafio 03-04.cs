0036916bd8f762a22ff4013e512b9656df0daa53
Carlos-250901 <51840942+Carlos-250901@users.noreply.github.com>
2020-01-26 16:17:48 -0500

    Add files via upload

---
+++ Desafio 03-04.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace desafio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+                Console.WriteLine("Ingrese datos w,t,c");
+                double w = double.Parse(Console.ReadLine());
+                double t = double.Parse(Console.ReadLine());
+                double c = double.Parse(Console.ReadLine());
+            double cr = c*(Math.PI/180.0);
+            Console.WriteLine(cr);
+            double e = Math.PI-cr;
+            double d = Math.Asin((t * Math.Sin(e)) / (w));
+            
+            double b = Math.PI-d-e ;
+           
+                double x = (w / Math.Sin(e)) * Math.Sin(b);
+
+                ; Console.WriteLine("x es " + x);
+            }
+        }
+    }
+
---
+++ Desafio03-02.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tarea2
+{
+    class Desafio0
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese datos w,t,c");
+            double w = double.Parse(Console.ReadLine());
+            double t = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double cr = c*(Math.PI/180);
+            
+            double e = 180.0-cr;
+            double d = Math.Asin((t*Math.Sin(e))/w);
+            double x = (w/Math.Sin(e))*Math.Sin(d);
+
+;            Console.WriteLine("x es " + x);
+        }
+    }
+}
