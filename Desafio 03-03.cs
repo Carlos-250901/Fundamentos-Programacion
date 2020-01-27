34686abee5812ece47170248576092922efbe2c2
Carlos-250901 <51840942+Carlos-250901@users.noreply.github.com>
2020-01-26 20:00:34 -0500

    Add files via upload

---
+++ Desafio 03-03.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Desafio0304
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese datos w,d,x");
+            double w = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+            double dr = d * (Math.PI / 180);
+            double z = (w / Math.Sin(Math.PI/2)) * (Math.Sin(dr));
+
+            
+            double catetoxy = Math.Sqrt(w*w-z*z) ;
+            
+            double y = catetoxy-x;
+            Console.WriteLine("y es " + y);
+
+        }
+    }
+}
