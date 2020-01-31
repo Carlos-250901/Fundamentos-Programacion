61ffbcc074da56bacc436ff6b842a45a92a401ae
Carlos-250901 <51840942+Carlos-250901@users.noreply.github.com>
2020-01-30 23:15:49 -0500

    Add files via upload

---
+++ Desafio 04-02.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Desafio_04_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario");
+            int tarifa = int.Parse(Console.ReadLine());
+            if (tarifa < 877803 * 2)
+            {
+                Console.WriteLine("Paga tarifa A y su cuota moderadora es $3400");
+            }
+            else if (tarifa >= 877803 * 2 && tarifa < 877803 * 5)
+            {
+                Console.WriteLine("Paga tarifa B y su cuota moderadora es $13500");
+            }
+            else
+            {
+                Console.WriteLine("Paga tarifa C y su cuota moderadora es $35600");
+            }
+        }
+     }
+}
