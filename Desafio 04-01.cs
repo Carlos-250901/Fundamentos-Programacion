0348e896fcf708fb4a23b2bf3fa0ea7885b39cb2
Carlos-250901 <51840942+Carlos-250901@users.noreply.github.com>
2020-01-30 22:56:33 -0500

    Add files via upload

---
+++ Desafio 04-01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafio_04_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario");
+            int tarifa = int.Parse(Console.ReadLine());
+            if (tarifa < 877803*2)
+            {
+                Console.WriteLine("Paga tarifa A");
+            }
+            else if (tarifa >= 877803 * 2 && tarifa < 877803 * 4)
+            {
+                Console.WriteLine("Paga tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Paga tarifa C");
+            }
+         
+        }
+    }
+}
